7e02be806b415bb9e1771b4e3c111bf45f0ad944
Nina Zhao <nina.r.zhao@gmail.com>
2025-02-16 01:53:32 +0000

    chore: use vite env variables

--- src/utils/uphold-sdk.js
+++ src/utils/uphold-sdk.js
@@ -2,8 +2,8 @@ import SDK from "@uphold/uphold-sdk-javascript";
 
 const upholdSdk = new SDK({
   baseUrl: "http://api-sandbox.uphold.com",
-  clientId: "foo",
-  clientSecret: "bar",
+  clientId: import.meta.env.VITE_UPHOLD_CLIENT_ID,
+  clientSecret: import.meta.env.VITE_UPHOLD_CLIENT_SECRET,
 });
 
 export default upholdSdk;
